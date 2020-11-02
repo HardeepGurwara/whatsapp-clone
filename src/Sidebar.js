208bbf7d8df6b5c28f66961ac0c97fe84f66b089
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-11-02 23:39:04 +0000

    create the side header and imported material UI icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,29 @@
+import { Avatar, IconButton } from "@material-ui/core";
 import React from "react";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 import "./Sidebar.css";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>im side bar</h1>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
     </div>
   );
 }
