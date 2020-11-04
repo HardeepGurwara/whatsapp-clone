d49ac4b32113e3123e7b9ba2713c71c966ec8f39
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-11-04 23:46:50 +0000

    styled the chat component and will start working on the app logic

--- src/Chat.js
+++ src/Chat.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 function Chat() {
   const [seed, setSeed] = useState("");
@@ -31,9 +33,22 @@ function Chat() {
           </IconButton>
         </div>
       </div>
-      <div className="chat__body"></div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">hardeep </span>
+          hey guys
+          <span className="chat__timestamp">10:59pm</span>
+        </p>
+      </div>
 
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submi">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
